e5f9c157d5825f2eeb37cd4e5ba15f4442185ea6
Sachin <sachiinprajapatii@gmail.com>
2026-05-30 12:04:25 +0530

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+    <>
+    
+    <div className='items-center justify-center'>
+        <hr className='border-black' />
+        <h1 className='text-center text-sm  py-3'>  copyright &C 2026 . All right reserved by TYS Industries Ltd</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default Footer
